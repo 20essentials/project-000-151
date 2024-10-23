a9702641a84396a31437f2a6ddfcd1b677c154a9
20essentials <s0l0estudi4r@gmail.com>
2024-10-23 16:48:13 +0200

    v0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './global.css';
+
+const App = () => {
+  return (
+    <aside className='cube'>
+      <div className='side front'></div>
+      <div className='side behind'></div>
+      <div className='side right'></div>
+      <div className='side left'></div>
+      <div className='side top'></div>
+      <div className='side bottom'></div>
+    </aside>
+  );
+};
+
+export default App;
